41133e4cb375c088960cf4aa1f3cf74eb2da2ef3
Deheng Song <dhsong@vt.edu>
2019-08-07 15:00:25 -0400

    a

--- init.el
+++ init.el
@@ -1,73 +1,153 @@
-(require 'package)
-(let* ((no-ssl (and (memq system-type '(windows-nt ms-dos))
-                    (not (gnutls-available-p))))
-       (proto (if no-ssl "http" "https")))
-  (when no-ssl
-    (warn "\
-Your version of Emacs does not support SSL connections,
-which is unsafe because it allows man-in-the-middle attacks.
-There are two things you can do about this warning:
-1. Install an Emacs version that does support SSL and be safe.
-2. Remove this warning from your init file so you won't see it again."))
-  ;; Comment/uncomment these two lines to enable/disable MELPA and MELPA Stable as desired
-  (add-to-list 'package-archives (cons "melpa" (concat proto "://melpa.org/packages/")) t)
-  ;;(add-to-list 'package-archives (cons "melpa-stable" (concat proto "://stable.melpa.org/packages/")) t)
-  (when (< emacs-major-version 24)
-    ;; For important compatibility libraries like cl-lib
-    (add-to-list 'package-archives (cons "gnu" (concat proto "://elpa.gnu.org/packages/")))))
+;; User details
+(setq user-full-name "Deheng Song")
+(setq user-mail-address "dhsong@vt.edu")
+
+;; Package management
+(load "package")
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/") t)
 (package-initialize)
+;; (package-refresh-contents)
 
-(elpy-enable)
+;; Define default packages
+(defvar dhsong/packages '(
+			  auctex
+                          auto-complete
+                          autopair
+;;                          elpy
+;;                          flycheck
+;;                          magit
+                          markdown-mode
+                          org
+                          pdf-tools
+;;                          powerline
+;;                          smex
+                          solarized-theme
+			  writegood-mode
+			  yasnippet
+                          )
+  "Default packages")
 
-(require 'yasnippet)
-(yas-global-mode 1)
-
-(require 'reftex)
-(add-hook 'LaTeX-mode-hook 'turn-on-reftex)   ; with AUCTeX LaTeX mode
-(add-hook 'latex-mode-hook 'turn-on-reftex)   ; with Emacs latex mode
-(setq reftex-plug-into-AUCTeX t)
-
-(recentf-mode 1)
-(setq recentf-max-menu-items 25)
-(global-set-key "\C-x\ \C-r" 'recentf-open-files)
-
-(add-hook 'ein:ipynb-mode-hook 'ein:process-find-file-callback)
-
- ;; to have the buffer refresh after compilation
-(add-hook 'TeX-after-compilation-finished-functions
-        #'TeX-revert-document-buffer)
-
-(require 'org)
-(setq org-log-done 'time)
+;; Install default packages
+;;(defun dhsong/packages-installed-p ()
+;;  (loop for pkg in dhsong/packages
+;;        when (not (package-installed-p pkg)) do (return nil)
+;;        finally (return t)))
 ;;
-;; Standard key bindings
-(global-set-key "\C-cl" 'org-store-link)
-(global-set-key "\C-ca" 'org-agenda)
-(global-set-key "\C-cb" 'org-iswitchb)
+;;(unless (dhsong/packages-installed-p)
+;;  (message "%s" "Refreshing package database...")
+;;  (package-refresh-contents)
+;;  (dolist (pkg dhsong/packages)
+;;    (when (not (package-installed-p pkg))
+;;      (package-install pkg))))
+(mapc #'(lambda (package)
+	  (unless (package-installed-p package)
+	    (package-refresh-contents)
+	    (package-install package)))
+      dhsong/packages)
+
+;; Start-up options
+;;;; Splash Screen
+(setq inhibit-splash-screen t
+      initial-scratch-message nil
+      initial-major-mode 'org-mode)
+;;;; Scroll bar, Tool bar, Menu bar
+(scroll-bar-mode -1)
+(tool-bar-mode -1)
+(menu-bar-mode -1)
+;;;; Marking text
+(delete-selection-mode t)
+(transient-mark-mode t)
+(setq x-select-enable-clipboard t)
+;;;; Display settings
+(setq-default indicate-empty-lines t)
+(when (not indicate-empty-lines)
+  (toggle-indicate-empty-lines))
+;;;; Indentation
+(setq tab-width 2
+      indent-tabs-mode nil)
+;;;; Backup files
+(setq make-backup-files nil)
+;;;; Yes and No
+(defalias 'yes-or-no-p 'y-or-n-p)
+;;;; Key bindings
+(global-set-key (kbd "RET") 'newline-and-indent)
+(global-set-key (kbd "C-;") 'comment-or-uncomment-region)
+(global-set-key (kbd "M-/") 'hippie-expand)
+(global-set-key (kbd "C-+") 'text-scale-increase)
+(global-set-key (kbd "C--") 'text-scale-decrease)
+(global-set-key (kbd "C-c C-k") 'compile)
+(global-set-key (kbd "C-x g") 'magit-status)
+;;;; Misc
+(setq echo-keystrokes 0.1
+      use-dialog-box nil
+      visible-bell t)
+(show-paren-mode t)
+
+;; Org
+;;;; Settings
+(setq org-log-done t
+      org-todo-keywords '((sequence "TODO" "INPROGRESS" "DONE"))
+      org-todo-keyword-faces '(("INPROGRESS" . (:foreground "blue" :weight bold))))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (flyspell-mode)))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (writegood-mode)))
+
+;; Utilities
+;;;; Ido
+(ido-mode t)
+(setq ido-enable-flex-matching t
+      ido-use-virtual-buffers t)
+;;;; Column number mode
+(setq column-number-mode t)
+;;;; Temporary file management
+(setq backup-directory-alist `((".*" . ,temporary-file-directory)))
+(setq auto-save-file-name-transforms `((".*" ,temporary-file-directory t)))
+;;;; autopair-mode
+(require 'autopair)
+;;;; auto-complete
+(require 'auto-complete-config)
+(ac-config-default)
+
+;;;; Indentation and buffer cleanup
+(defun untabify-buffer ()
+  (interactive)
+  (untabify (point-min) (point-max)))
+
+(defun indent-buffer ()
+  (interactive)
+  (indent-region (point-min) (point-max)))
+
+(defun cleanup-buffer ()
+  "Perform a bunch of operations on the whitespace content of a buffer."
+  (interactive)
+  (indent-buffer)
+  (untabify-buffer)
+  (delete-trailing-whitespace))
+
+(defun cleanup-region (beg end)
+  "Remove tmux artifacts from region."
+  (interactive "r")
+  (dolist (re '("\\\\│\·*\n" "\W*│\·*"))
+    (replace-regexp re "" nil beg end)))
+
+(global-set-key (kbd "C-x M-t") 'cleanup-region)
+(global-set-key (kbd "C-c n") 'cleanup-buffer)
+
+(setq-default show-trailing-whitespace t)
+;;;;;
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-source-correlate-start-server t)
- '(TeX-view-program-selection
-   (quote
-    (((output-dvi has-no-display-manager)
-      "dvi2tty")
-     ((output-dvi style-pstricks)
-      "dvips and gv")
-     (output-dvi "xdvi")
-     (output-pdf "PDF Tools")
-     (output-html "xdg-open"))))
- '(doc-view-continuous t)
- '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/Documents/todo.org")))
  '(package-selected-packages
    (quote
-    (markdown-mode ein elpy pdf-tools yasnippet-snippets yasnippet org better-defaults auctex)))
- '(read-file-name-completion-ignore-case t))
-
+    (solarized-theme smex powerline pdf-tools markdown-mode magit flycheck elpy autopair auto-complete auctex))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
